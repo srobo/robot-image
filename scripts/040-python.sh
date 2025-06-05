dbc682ee2df5022e9108e679ac9adbb9ad412024
Jake Howard <git@theorangeone.net>
2025-06-05 22:34:10 +0100

    Don't install build-essential
    We probably don't need it, since all the packages we install are wheels

--- scripts/040-python.sh
+++ scripts/040-python.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
-# Package install for python libraries
-apt-get -y install \
-    cmake \
-    build-essential \
-    python3-pip
+# Install pip
+apt-get -y install python3-pip
 
 chmod +x /tmp/packer/python/install-packages.sh
 /tmp/packer/python/install-packages.sh
