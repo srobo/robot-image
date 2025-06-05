9781ac97108f0724541f8a1cc70efc5081da30ef
Jake Howard <git@theorangeone.net>
2025-06-05 22:31:22 +0100

    Disable pip cache when installing
    This should make the images much smaller

--- files/python/install-packages.sh
+++ files/python/install-packages.sh
@@ -2,10 +2,10 @@
 set -eux -o pipefail
 
 # Install base python packages
-pip install -r /tmp/packer/python/requirements.txt
+pip install --no-cache-dir -r /tmp/packer/python/requirements.txt
 
 # Install additional useful libraries for the competitors
-pip install -r /tmp/packer/python/libraries.txt
+pip install --no-cache-dir -r /tmp/packer/python/libraries.txt
 
 # Record the installed packages
 pip freeze > py-packages.txt
