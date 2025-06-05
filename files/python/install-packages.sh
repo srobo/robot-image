34c420a6f3fdaa56d3296cb8d36bc3a4feee2f86
Jake Howard <git@theorangeone.net>
2025-06-05 22:06:33 +0100

    Use system Python

--- files/python/install-packages.sh
+++ files/python/install-packages.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eux -o pipefail
 
-# Load pyenv python into path
-source ~/.profile
-
 pip install --upgrade pip setuptools wheel
 
 # Install base python packages
@@ -11,9 +8,6 @@ pip install -r /tmp/packer/python/requirements.txt
 
 # Install additional useful libraries for the competitors
 pip install -r /tmp/packer/python/libraries.txt
-
-# Make the entrypoint folder
-mkdir -p ~/.local/bin
 
 # Record the installed packages
 pip freeze > py-packages.txt
